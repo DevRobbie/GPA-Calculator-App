13b22c4cb656ac90963715a7ef14892a89366ed0
robbie <robbiebeall2@icloud.com>
2025-02-17 20:20:53 -0500

    changed title changing color to it be a hat icon at the top. Also, made it more spaced out a little it. Also added some things like unweighted gpa calculator

--- GPA Calculator App/ContentView.swift
+++ GPA Calculator App/ContentView.swift
@@ -7,10 +7,18 @@ struct ContentView: View {
     @State private var totalClasses: Double = 0
     @State private var gpa: Double = 0.0
     @State private var colorFont: Color = .black
+    @State private var uwPoints: Double = 0.0
     var body: some View {
         var gpa: Double {
                 if totalClasses > 0 {
                     return totalPoints / totalClasses
+                } else {
+                    return 0.0
+                }
+            }
+        var uwGPA: Double {
+                if totalClasses > 0 {
+                    return uwPoints / totalClasses
                 } else {
                     return 0.0
                 }
@@ -27,10 +35,18 @@ struct ContentView: View {
             }
         }
         VStack(spacing:25) {
+            Spacer()
+            Spacer()
+            Spacer()
+            Image(systemName: "graduationcap.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(colorFont)
+            Spacer()
             Text("GPA Calculator")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-                .foregroundStyle(colorFont)
+                .foregroundStyle(.black)
             Text("Class Grades:")
                 .font(.headline)
             
@@ -38,22 +54,27 @@ struct ContentView: View {
                 Button("A") {
                     totalPoints += 4
                     totalClasses += 1
+                    uwPoints += 4
                 }
                 Button("B") {
                     totalPoints += 3
                     totalClasses += 1
+                    uwPoints += 3
                 }
                 Button("C") {
                     totalPoints += 2
                     totalClasses += 1
+                    uwPoints += 2
                 }
                 Button("D") {
                     totalPoints += 1
                     totalClasses += 1
+                    uwPoints += 1
                 }
                 Button("F") {
                     totalPoints += 0
                     totalClasses += 1
+                    uwPoints += 0
                 }
             }
             .buttonStyle(.borderedProminent)
@@ -69,8 +90,11 @@ struct ContentView: View {
             .buttonStyle(.borderedProminent)
             HStack{
                 Text("GPA: \(String(format: "%.2f", gpa))")
-                Text("Total Classes: \(Int(totalClasses))")
+                Text("Unweighted GPA: \(String(format: "%.2f", uwGPA))")
             }//later add unweighted gpa here and move total classes a line down
+            HStack{
+                Text("Total Classes: \(Int(totalClasses))")
+            }
             HStack {
                 Button("Reset") {
                     totalPoints = 0
@@ -81,6 +105,7 @@ struct ContentView: View {
             
             
         }
+        Spacer()
         
         .padding()
     }
